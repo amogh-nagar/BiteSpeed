0ddcd690ccb510b6819e1e843bee56f0be3f94f9
Amoghtech <amoghnagarpersonal@gmail.com>
2024-06-15 16:06:11 +0530

    MYSQL Configuration changes

--- utils/connection.js
+++ utils/connection.js
@@ -12,7 +12,7 @@ module.exports = {
       host: process.env.MYSQL_HOST,
       user: process.env.MYSQL_USER,
       password: process.env.MYSQL_PASSWORD,
-      database: "bitespeed",
+      database: process.env.MYSQL_DB,
       multipleStatements: true
     });
     dbConn.connect(function (err) {
